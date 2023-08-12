d9e7bde3e8f5e41cf577636fa9b83bc7c2ee4ffd
Igor Sverchkov <knjaz1989@gmail.com>
2023-08-13 02:44:36 +0300

    bad gotovchenko 1

--- app/bot/depedencies.py
+++ app/bot/depedencies.py
@@ -6,8 +6,6 @@ from settings import config
 def check_ip(request: Request):
     if request.client:
         host = request.client.host
-        port = request.client.port
-        print(f"-------->Важная инфа<-------{host}, {port}")
         if host == config.ALLOWED_HOST:
             return
     raise fastapi.HTTPException(
